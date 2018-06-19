bdf584c12f4fd5055cd524cc0da2d108cdd97319
Joe Hitchens <joe@sleepless.com>
2018-06-18 20:49:19 -0700

    -

--- freekey.js
+++ freekey.js
@@ -44,25 +44,27 @@ function del(key, cb){
 
 // XXX this should really be an independent module that uses freekey, but whatever
 // Wrapper that encrypts the stored values
+// "alg" is optional encryption algorithm passed to node.js crypto module.
 function crypt(password, alg) {
 	const crypto = require("crypto");
-	const o = module.exports;
+	const o = this;
 	alg = alg || "aes-256-cbc";
 	return {
 		get: function(k, cb) {
 			return o.get(k, (enc)=> {
 				const d = crypto.createDecipher(alg, password);
-				let json = d.update(enc, "hex", "utf8") + d.final("utf8");;
+				let json = d.update(enc, "base64", "utf8") + d.final("utf8");;
 				cb(j2o(json));
 			});
 		},
 		put: function(k, v, cb) {
 			let json = o2j(v);
 			const c = crypto.createCipher(alg, password);
-			let enc = c.update(json, "utf8", "hex") + c.final("hex");;
+			let enc = c.update(json, "utf8", "base64") + c.final("base64");;
 			return o.put(k, enc, cb);
 		},
 		del: o.del,
+		crypt: o.crypt,
 		prefix: o.prefix
 	};
 }
@@ -78,11 +80,12 @@ function crypt(password, alg) {
 //      fk.set("sleepless_foo", "bar", ...)
 //      fk.get("sleepless_foo", ...)
 function prefix(pre) {
-	let o = module.exports;
+	const o = this;
 	return {
 		get: function(k, cb) { return o.get(pre+k, cb); },
 		put: function(k, v, cb) { return o.put(pre+k, v, cb); },
 		del: function(k, cb) { return o.del(pre+k, cb); },
+		crypt: o.crypt,
 		prefix: o.prefix
 	};
 }
